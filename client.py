9ab636c3a3e8cef7c6d66aa2c44ad840567c826e
Rohit Nagraj <rohitnagraj.99@gmail.com>
2019-07-11 16:28:02 +0530

    Added client skeleton

---
+++ client.py
@@ -0,0 +1,24 @@
+import pickle
+import pygame
+
+from network import Network
+
+pygame.font.init()
+
+width = 700
+height = 700
+
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
+
+
+def redraw_window(win, game, p):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
