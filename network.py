a6b96b09c1d69c5b5c0a10f85682830a84f0e2c7
Rohit Nagraj <rohitnagraj.99@gmail.com>
2019-07-11 14:38:16 +0530

    Added docstrings to network

--- network.py
+++ network.py
@@ -3,17 +3,28 @@ import socket
 
 
 class Network:
+    """
+    Interfaces the socket server for the client
+    """
     def __init__(self):
+
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.0.102"
-        self.port = 5555
+        self.server = "192.168.0.102"    # Local IP address of the server
+        self.port = 5555                 # Port on which the server is running
         self.addr = (self.server, self.port)
-        self.p = self.connect()
+        self.p = self.connect()          # Player number
 
     def get_p(self):
+        """
+        :return: [0, 1] player number
+        """
         return self.p
 
     def connect(self):
+        """
+        Connects the new client to the server
+        :return: The player number assigned by the server
+        """
         try:
             self.client.connect(self.addr)
             return self.client.recv(2048).decode()
@@ -21,6 +32,11 @@ class Network:
             print(e)
 
     def send(self, data):
+        """
+        Send data from client to server and receive data from server
+        :param data: (string) data to send
+        :return: (game instance) updated game instance
+        """
         try:
             self.client.send(str.encode(data))
             return pickle.loads(self.client.recv(2048))
